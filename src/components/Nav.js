904b11f66ac5b0705aeab4c4d4ba88ee1cddd38d
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2022-03-03 17:16:34 -0600

    updated styles for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,32 +6,49 @@ import 'tachyons';
 function Nav(props) {
     const tabs = ["About", 'Albums', "Samples", "Dashboard", "Socials"];
     return (
-<div class="fixed w-100 ph3 pv3 pv4-ns ph4-m ph5-l">
-  <nav>
-        <ul className="flex-row" id="nav">
+      <div>
+        <nav className="pa1 pa3-ns flex-row" id="nav-items">
           {tabs.map((tab) => (
-            <li
-              className={
-                props.currentPage === tab ? "nav-item navActive" : "nav-item"
-              }
-              key={tab}
+            <a href={"#" + tab.toLowerCase()}
+                
+            onClick={() => props.handlePageChange(tab)}
+            className={
+              props.currentPage === tab ? "nav-link active" : "nav-link"
+                    }
             >
-              <a
-                href={"#" + tab.toLowerCase()}
-
-                onClick={() => props.handlePageChange(tab)}
-                className={
-                  props.currentPage === tab ? "nav-link active" : "nav-link"
-                }
-              >
                 {tab}
-              </a>
-            </li>
+            </a>
           ))}
-        </ul>
         </nav>
-  </div>
-    );
+      </div>
+    )
+
+// {/* <div>
+//   <nav class="navbar">
+//       <ul className="flex-row" id="nav">
+//           {tabs.map((tab) => (
+//             <li
+//               className={
+//                 props.currentPage === tab ? "nav-item  navActive" : "nav-item  "
+//               }
+//               key={tab}
+//             >
+//               <a
+//                 href={"#" + tab.toLowerCase()}
+
+//                 onClick={() => props.handlePageChange(tab)}
+//                 className={
+//                   props.currentPage === tab ? "nav-link active" : "nav-link"
+//                 }
+//               >
+//                 {tab}
+//               </a>
+//             </li>
+//           ))}
+//         </ul>
+//         </nav>
+//   </div> */}
+//     );
 }
 
 export default Nav;
