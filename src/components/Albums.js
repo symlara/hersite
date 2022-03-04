c768e118643de0dc60f652d592f34dee6d341ab1
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2022-03-03 19:00:05 -0600

    added albums to albums page

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -2,6 +2,7 @@ import React from 'react';
 import 'tachyons';
 import image2 from '../assets/images/H.E.R._(2017)_album_cover.jpg';
 import image3 from '../assets/images/I_Used_To_Know_Her.jpg';
+import image4 from '../assets/images/Back_Of_My_Mind_By_H.E.R.png';
 import '../../src/index.css'
 
 function Albums () {
@@ -67,6 +68,31 @@ function Albums () {
                     </ul>
                   </div>
                 </article>
+
+                {/* <!-- Back Of My Mind album --> */}
+              <article class="br2 ba white-60 mv4 w-100 w-50-m w-25-l mw5 center" id="volume-3">
+                <img src={image4} alt="Back of My Mind album" />
+                <h1>Back Of My Mind</h1>
+                <div class="ph1">
+                <ul class="list pl0 ml0 center mw6 ba white br2 h-100">
+                      <li class="ph3 pv3 bb b--light-silver">We Made It</li>
+                      <li class="ph3 pv3 bb b--light-silver">Back of My Mind (Ft. Ty Dolla $ign)</li>
+                      <li class="ph3 pv3 bb b--light-silver">Trauma (Ft. Cordae)</li>
+                      <li class="ph3 pv3 bb b--light-silver">Damage</li>
+                      <li class="ph3 pv3 bb b--light-silver">Find A Way (Ft. Lil Baby)</li>
+                      <li class="ph3 pv3 bb b--light-silver">Bloody Waters</li>
+                      <li class="ph3 pv3 bb b--light-silver">Closer To Me</li>
+                      <li class="ph3 pv3 bb b--light-silver">Come Through (Ft. Chris Brown)</li>
+                      <li class="ph3 pv3 bb b--light-silver">My Own</li>
+                      <li class="ph3 pv3 bb b--light-silver">Lucky</li>
+                      <li class="ph3 pv3 bb b--light-silver">Cheat Code</li>
+                      <li class="ph3 pv3 bb b--light-silver">Process</li>
+                      <li class="ph3 pv3 bb b--light-silver">Hard To Love</li>
+                      <li class="ph3 pv3 bb b--light-silver">I Can Havce It All (Ft. DJ Khaled)</li>
+
+                  </ul>
+                </div>
+              </article>
         </section>
     );
 
